eb5bfa46f61c9e81b1aa0ab7f45c8bf3642d8640
zhaleh <zhalehelmi@gmail.com>
2024-05-15 20:42:59 +0330

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+      <button>New Game</button>
+    </div>
+  );
+};
+
+export default App;
