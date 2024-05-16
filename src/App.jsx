f8b5fb9bdafb8ea91360d74fb75529556d7e5b13
zhaleh <zhalehelmi@gmail.com>
2024-05-16 17:20:41 +0330

    add card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import cardImages from "./Data";
+import SingleCard from "./components/SingleCard";
 
 const App = () => {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => {
+        return Math.random() - 0.5;
+      })
+      .map((card) => ({ ...card, id: Math.random() }));
+    setCards(shuffleCards);
+    setTurns(0);
+  };
+
   return (
     <div className="App">
       <h1>Memory Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard key={card.id} {...card} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SingleCard.css";
+
+const SingleCard = ({ src }) => {
+  return (
+    <div className="card">
+      <div>
+        <img className="front" src={src} alt="card front" />
+        <img className="back" src="/img/cover.png" alt="card back" />
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
